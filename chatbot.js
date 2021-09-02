304b107501ad8d1124cb45a11ee3b96de40cd774
CPKaz <ck7aj@virginia.edu>
2021-09-02 14:27:45 -0400

    got bot timer working, changed css

--- chatbot.js
+++ chatbot.js
@@ -5,9 +5,13 @@ var human_pre = '<p class="human_text"><span>';
 var post = '</span></p>';
 // var todo_and_timer_said = false; hide the buttons
 // var background = chrome.extension.getBackgroundPage();
-yes_and_no = '<button class="binary">Yes</button>\n <button class="binary">No</button>'
+yes_and_no = '<span id="b_buttons"> <button class="binary">Yes</button>\n <button class="binary">No</button></span>'
 
 var timer_binary = false;
+
+let bot_words = "Okay, buddy";
+words = bot_pre + bot_words + post;
+chat(words, context);
 
 function chat(words, context = $('#chatbox')){
     context.append(words);
@@ -26,36 +30,70 @@ function change_list(q){
 
 
 // context = document.querySelector("#chatbox");
-let bot_words = "Okay, buddy";
-words = bot_pre + bot_words + post;
-chat(words, context);
 $("#todo_and_timer").on("click", function(){
     let b_text = $("#todo_and_timer").text()
     let reply = human_pre + $("#todo_and_timer").text() + post
     chat(reply);
+
     ask_for_timer();
+    $("#init_buttons").empty();
 
     // console.log(JSON.parse(localStorage.getItem('todo_lists')));
     // change_list("cash");
 });
 
-$(".binary").on("click", function(e){
-    let b_text = e.target.text();
-    console.log(b_text);
+$("#chatbox").on("click", '.binary', function(e){
+    // console.log('text_below');
+    let b_text = $(this).text();
+    chat(human_pre + b_text + post);
     if (b_text == "No"){
-        chat(bot_pre + "Okay." +post);
-
+        timer_binary = false;
+        chat(bot_pre + "Okay." + post);
     }
     else if (b_text == "Yes"){
         if (timer_binary){
             timer_binary = false;
-            chat(bot_pre + "What time would you like to set? (mm:ss or ssss)" + post);
+            chat('<p class="bot_text" id="bot_time_setter"><span>' + "What time would you like to set?" + post);
+            chat('<span>' + '<input class="human_text" id="time_input">' + '</span>');
         }
     }
     else {
         chat(bot_pre + "check logs" +post)
     }
+    $("#b_buttons").empty();
 
+});
+
+$("#chatbox").on("keypress", '#time_input', function(e){
+   if (e.which == 13){ // will this work on added html?
+        time = $(this).val()
+    if (/(^([0-5]?[0-9]?):[0-5][0-9]$)|(^\d{1,4}$)/.test(time)){
+        if (time.includes(':')){
+            l = time.length;
+            if (l == 3){
+                time = '00' + time;
+            }
+            if (l == 4){
+                time = '0' + time;
+            }
+            timer = parseInt(time.substring(0, 2))*60 + parseInt(time.substring(3, 5));
+            chatbot_set_timer(timer);
+        }
+        else {
+            timer = parseInt(time);
+            if (timer > 60**2 - 1){
+                timer = 60**2 - 1
+            }
+            chatbot_set_timer(timer);
+    }
+    
+   }
+   else {
+
+    alert(time);
+    console.log(time);
+}
+}
 });
 
 function ask_for_timer(context = $('#chatbox')){
@@ -63,12 +101,10 @@ function ask_for_timer(context = $('#chatbox')){
     let bot_reply = "Would you like me to set a break timer?"
     chat(bot_pre + bot_reply + post);
     timer_binary = true;
-    chat(yes_and_no)
+    chat(yes_and_no);
 }
 
-
-
-function set_timer(duration){
+function chatbot_set_timer(duration){
     localStorage.setItem('time_left', duration);
     localStorage.setItem('active', true);
     let message = {msg: "start_clock"};
